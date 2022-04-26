79f9f94948a977dc810add8db7b86868bc04ab15
nairdesigns <nairbdg@gmail.com>
2022-04-26 17:19:13 +0800

    nav bar is white!

--- src/components/navbar/NavbarElements.js
+++ src/components/navbar/NavbarElements.js
@@ -73,6 +73,8 @@ export const NavMenu = styled.ul`
   list-style: none;
   text-align: center;
   margin-right: -22px;
+  background-color: white;
+
   height: 80px;
 
   @media screen and (max-width: 768px) {
