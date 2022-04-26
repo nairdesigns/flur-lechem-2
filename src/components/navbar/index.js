3e7712350ab21c95a61556b4e270b9209ae0200f
nairdesigns <nairbdg@gmail.com>
2022-04-26 16:40:03 +0800

    removed sign in and sign up button in main nav bar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -55,13 +55,7 @@ export const Navbar = ({ toggle }) => {
             <NavItem>
               <NavLinks to="services">Services</NavLinks>
             </NavItem>
-            <NavItem>
-              <NavLinks to="signup">Sign Up</NavLinks>
-            </NavItem>
           </NavMenu>
-          <NavBtn>
-            <NavBtnLink to="/signin">Sign In</NavBtnLink>
-          </NavBtn>
         </NavbarContainer>
       </Nav>
     </>
