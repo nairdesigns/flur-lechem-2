536556ada4337799dd0934b1b0d653d4a97ee554
nairdesigns <nairbdg@gmail.com>
2022-05-05 10:44:21 +0800

    fixed lgogo problem

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -13,6 +13,7 @@ import {
 } from "./NavbarElements";
 import "./../../App.css";
 import logo from "../../images/flur-lechem-logo.jpg";
+import logo2 from "../../images/logo.jpg";
 export const Navbar = ({ toggle }) => {
   const [scrollNav, setScrollNav] = useState(false);
   const changeNav = () => {
@@ -32,12 +33,7 @@ export const Navbar = ({ toggle }) => {
       <Nav scrollNav={scrollNav}>
         <NavbarContainer>
           <NavLogo to="/">
-            <img
-              className="logo-text"
-              src="https://scontent.fcrk4-1.fna.fbcdn.net/v/t39.30808-6/277730334_102251775781603_2943809287366827913_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=73z0x8kSZI0AX_XnLGM&_nc_zt=23&_nc_ht=scontent.fcrk4-1.fna&oh=00_AT_tvqDvstku3V0wzMw_lRvA8aTzDRRe_vl1BlFHrjZmDg&oe=626BBBF7"
-              alt="logo"
-              height="70px"
-            />
+            <img className="logo-text" src={logo2} alt="logo" height="70px" />
             <h1 className="logo-text">
               <img src={logo} height="60px" alt="logo" />
             </h1>
